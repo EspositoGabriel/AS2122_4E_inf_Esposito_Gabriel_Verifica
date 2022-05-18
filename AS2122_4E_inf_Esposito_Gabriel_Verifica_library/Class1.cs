b42193231db87c9d3d5dc8971e1cda05ea67d1ab
EspositoGabriel <gabriespo2010@gmail.com>
2022-05-18 10:13:31 +0200

    .

--- AS2122_4E_inf_Esposito_Gabriel_Verifica_library/Class1.cs
+++ AS2122_4E_inf_Esposito_Gabriel_Verifica_library/Class1.cs
@@ -15,11 +15,27 @@ namespace AS2122_4E_inf_Esposito_Gabriel_Verifica_library
 
 
         public string Codice { get { return codice; } }
-        public string Descrizione {get { return descrizione} }
-        public string Unitadimisura {get { return unitadimisura} }
-        public string Prezzo {get {return prezzo } }
+        public string Descrizione {get { return descrizione; } }
+        public string Unitadimisura {get { return unitadimisura; } }
+        public string Prezzo {get { return prezzo; } }
 
 
+        public Articolo(string codice, string descrizione, string unitadimisura, string prezzo)
+        {
+            this.codice = codice;
+            this.descrizione = descrizione;
+            this.unitadimisura = unitadimisura;
+            this.prezzo = prezzo;
+
+
+        }
+        public override string ToString()
+        {
+            return $"{descrizione} {unitadimisura} {prezzo} {codice}"; 
+
+
+            }
+        }
 
 
     }

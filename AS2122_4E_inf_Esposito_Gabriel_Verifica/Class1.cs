ecaa946530b895b0d1fbe2e5ffe73ca8409dcffe
EspositoGabriel <gabriespo2010@gmail.com>
2022-05-18 10:02:53 +0200

    creazione property

---
+++ AS2122_4E_inf_Esposito_Gabriel_Verifica/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS2122_4E_inf_Esposito_Gabriel_Verifica
+{
+    class Class1
+    {
+
+    }
+}
---
+++ AS2122_4E_inf_Esposito_Gabriel_Verifica_library/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2122_4E_inf_Esposito_Gabriel_Verifica_library
+{
+    public class Class1
+    {
+        string  codice;
+        string  descrizione;
+        string  unitadimisura;
+        string  prezzo;
+
+
+        public string Codice { get { return codice; } }
+        public string Descrizione {get { return descrizione} }
+        public string Unitadimisura {get { return unitadimisura} }
+        public string Prezzo {get {return prezzo } }
+
+
+
+
+    }
+    
+
+
+
+
+
+
+
+}
